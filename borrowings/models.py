b0c2addf4d53370627de460555e7fbb6c1e3054c
Svitlana Savikhina <savihina111@gmail.com>
2023-10-26 22:09:25 +0300

    Create models for borrowings app

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+from users.models import User
+
+
+class Borrow(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(blank=True, null=True)
+    book = models.ForeignKey(Book, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"Book {self.book.title} must be returned on {self.expected_return_date} by user {self.user.username}"
