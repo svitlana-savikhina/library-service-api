c369a57d4d350583341bafe93ccef97cce770f44
Svitlana Savikhina <savihina111@gmail.com>
2023-11-01 15:11:49 +0200

    Add save method and is_active computed property to the Borrow model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 
 from books.models import Book
@@ -5,11 +7,22 @@ from users.models import User
 
 
 class Borrow(models.Model):
-    borrow_date = models.DateField(auto_now_add=True)
+    borrow_date = models.DateField()
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(blank=True, null=True)
     book = models.ForeignKey(Book, on_delete=models.PROTECT)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
 
-    def __str__(self):
-        return f"Book {self.book.title} must be returned on {self.expected_return_date} by user {self.user.username}"
+    @property
+    def is_active(self):
+        today = date.today()
+        return self.actual_return_date is None or self.actual_return_date >= today
+
+    def save(self, *args, **kwargs):
+        super(Borrow, self).save(*args, **kwargs)
+        if self.actual_return_date:
+            self.book.inventory += 1
+            self.book.save()
+        else:
+            self.book.inventory -= 1
+            self.book.save()
