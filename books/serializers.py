452e89666c1a5d49d71b7c47a5b8524b54fa7a08
Svitlana Savikhina <savihina111@gmail.com>
2023-11-09 15:00:20 +0200

    Add BookDetailSerializer

--- books/serializers.py
+++ books/serializers.py
@@ -6,4 +6,10 @@ from books.models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
+        fields = ("id", "title", "author", "daily_fee")
+
+
+class BookDetailSerializer(BookSerializer):
+    class Meta:
+        model = Book
         fields = "__all__"
