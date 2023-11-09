7b6616d3a6373ceda824ca2f262be4dea6ef823e
Svitlana Savikhina <savihina111@gmail.com>
2023-11-09 14:57:16 +0200

    Add get_serializer_class to BookViewSet

--- books/views.py
+++ books/views.py
@@ -1,9 +1,7 @@
-from rest_framework import viewsets
-
-from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework import viewsets, permissions
 
 from books.models import Book
-from books.serializers import BookSerializer
+from books.serializers import BookSerializer, BookDetailSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
@@ -11,8 +9,20 @@ class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
 
     def get_permissions(self):
-        if self.action in ["list", "retrieve"]:
-            permission_classes = [AllowAny]
-        else:
-            permission_classes = [IsAdminUser]
-        return [permission() for permission in permission_classes]
+        if self.action == "list":
+            return [permissions.AllowAny()]
+        elif self.action in [
+            "create",
+            "update",
+            "partial_update",
+            "destroy",
+            "retrieve",
+        ]:
+            return [permissions.IsAdminUser()]
+
+        return [permissions.IsAuthenticated()]
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookSerializer
+        return BookDetailSerializer
