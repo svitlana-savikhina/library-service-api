0a3e3107fba1e053870c64fab88ced851cef0dd1
Svitlana Savikhina <savihina111@gmail.com>
2023-11-22 19:47:37 +0200

    Update serializers.py in borrowings app

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -14,7 +14,6 @@ class BorrowSerializer(serializers.ModelSerializer):
             "expected_return_date",
             "actual_return_date",
             "book",
-            "user",
             "is_active",
         )
 
@@ -27,3 +26,4 @@ class BorrowDetailSerializer(BorrowSerializer):
 class BorrowListSerializer(BorrowSerializer):
     book = serializers.SlugRelatedField(many=False, read_only=True, slug_field="title")
     user = serializers.SlugRelatedField(many=False, read_only=True, slug_field="email")
+
