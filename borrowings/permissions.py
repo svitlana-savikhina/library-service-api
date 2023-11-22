f630dff443a1adefad2874368adb37c894ba8584
Svitlana Savikhina <savihina111@gmail.com>
2023-11-22 19:45:45 +0200

    Update permissions.py

--- borrowings/permissions.py
+++ borrowings/permissions.py
@@ -7,8 +7,6 @@ class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.user.is_staff:
+        if (request.method in permissions.SAFE_METHODS) and request.user.is_staff:
             return True
         return obj.user == request.user
