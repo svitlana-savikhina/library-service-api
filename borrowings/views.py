ae7ebf94bdedfdba5ba589670829e6f8f59aa5ef
Svitlana Savikhina <savihina111@gmail.com>
2023-11-23 10:43:08 +0200

    Update views.py in borrowings app

--- borrowings/views.py
+++ borrowings/views.py
@@ -29,4 +29,4 @@ class BorrowViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer: BorrowSerializer) -> None:
         borrowing = serializer.save(user=self.request.user)
         text = f"New borrowing created: book- {borrowing.book.title}, user-{borrowing.user.email}"
-        send_borrowing_notification.delay(text)
+        send_borrowing_notification.apply_async(text)
